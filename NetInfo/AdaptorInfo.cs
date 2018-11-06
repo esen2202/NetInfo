f62271bd349925cb4c06e6e9e8e29e4a5770569c
Esen Erkan <erkanesen2202@gmail.com>
2018-11-07 00:16:07 +0300

    all info from adapters - ok

--- NetInfo/AdaptorInfo.cs
+++ NetInfo/AdaptorInfo.cs
@@ -1,31 +1,231 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace NetInfo
 {
-    class AdaptorInfo
+    class AdapterInfo
     {
-        static AdaptorInfo adaptorInfo;
-        private static object objLock;
+        private static AdapterInfo adapterInfo;
+        private static object objLock = new object();
 
-        NetworkInterface[] adapters;
+        private NetworkInterface[] adapters;
+        public List<AdapterObject> listAdapter;
 
-        private AdaptorInfo()
+        private AdapterInfo()
         {
-
+            adapters = NetworkInterface.GetAllNetworkInterfaces();
+            listAdapter = new List<AdapterObject>();
+            RefreshInfos();
         }
 
-        public static AdaptorInfo CreateInstance()
+        public static AdapterInfo CreateInstance()
         {
-            if (adaptorInfo != null)
+            if (adapterInfo == null)
                 lock (objLock)
                 {
-                    if (adaptorInfo != null)
-                        adaptorInfo = new AdaptorInfo();
+                    if (adapterInfo == null)
+                        adapterInfo = new AdapterInfo();
                 }
 
-            return adaptorInfo;
+            return adapterInfo;
         }
 
+        public void RefreshInfos()
+        {
+            listAdapter.Clear();
 
+            foreach (NetworkInterface adapter in adapters)
+            {
+                var ipProp = adapter.GetIPProperties();
+
+                foreach (UnicastIPAddressInformation ip in ipProp.UnicastAddresses)
+                {
+                    if (adapter.OperationalStatus == OperationalStatus.Up &&
+                        ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        IPInterfaceProperties adapterProp = adapter.GetIPProperties();
+                        IPv4InterfaceProperties adapterPropV4 = adapterProp.GetIPv4Properties();
+                        GatewayIPAddressInformationCollection gate = adapter.GetIPProperties().GatewayAddresses;
+
+                        listAdapter.Add(new AdapterObject
+                        {
+                            Description = adapter.Description,
+                            NetworkInterfaceType = adapter.NetworkInterfaceType.ToString(),
+                            PhysicalAddress = adapter.GetPhysicalAddress().ToString(),
+                            IsReceiveOnly = adapter.IsReceiveOnly,
+                            SupportMulticast = adapter.SupportsMulticast,
+
+                            IpAddress = ip.Address.ToString(),
+                            SubnetMask = ip.IPv4Mask.ToString(),
+                            Gateway = gate.Any() ? gate.FirstOrDefault().Address.ToString() : "",
+
+                            DnsSuffix = adapterProp.DnsSuffix,
+                            IsDnsEnabled = adapterProp.IsDnsEnabled,
+                            IsDynamicDnsEnabled = adapterProp.IsDynamicDnsEnabled,
+
+                            Index = adapterPropV4.Index,
+                            Mtu = adapterPropV4.Mtu,
+                            IsAutomaticPrivateAddressingActive = adapterPropV4.IsAutomaticPrivateAddressingActive,
+                            IsAutomaticPrivateAddressingEnabled = adapterPropV4.IsAutomaticPrivateAddressingEnabled,
+                            IsForwardingEnabled = adapterPropV4.IsForwardingEnabled,
+                            UsesWins = adapterPropV4.UsesWins
+                        });
+                    }
+                }
+
+            }
+        }
+    }
+
+    class AdapterObject
+    {
+
+        #region Base Info
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private string networkInterfaceType;
+
+        public string NetworkInterfaceType
+        {
+            get { return networkInterfaceType; }
+            set { networkInterfaceType = value; }
+        }
+
+        private string physicalAddress;
+
+        public string PhysicalAddress
+        {
+            get { return physicalAddress; }
+            set { physicalAddress = value; }
+        }
+
+        private bool isReceiveOnly;
+
+        public bool IsReceiveOnly
+        {
+            get { return isReceiveOnly; }
+            set { isReceiveOnly = value; }
+        }
+
+        private bool supportsMulticast;
+
+        public bool SupportMulticast
+        {
+            get { return supportsMulticast; }
+            set { supportsMulticast = value; }
+        }
+        #endregion
+
+        #region IP
+        private string ipAddress;
+
+        public string IpAddress
+        {
+            get { return ipAddress; }
+            set { ipAddress = value; }
+        }
+
+        private string subnetMask;
+
+        public string SubnetMask
+        {
+            get { return subnetMask; }
+            set { subnetMask = value; }
+        }
+
+        private string gateway;
+
+        public string Gateway
+        {
+            get { return gateway; }
+            set { gateway = value; }
+        }
+        #endregion
+
+        #region DNS
+        private string dnsSuffix;
+
+        public string DnsSuffix
+        {
+            get { return dnsSuffix; }
+            set { dnsSuffix = value; }
+        }
+
+        private bool isDnsEnabled;
+
+        public bool IsDnsEnabled
+        {
+            get { return isDnsEnabled; }
+            set { isDnsEnabled = value; }
+        }
+
+        private bool isDynamicDnsEnabled;
+
+        public bool IsDynamicDnsEnabled
+        {
+            get { return isDynamicDnsEnabled; }
+            set { isDynamicDnsEnabled = value; }
+        }
+        #endregion
+
+        #region IP4 Specific data
+
+        private int index;
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        private int mtu;
+
+        public int Mtu
+        {
+            get { return mtu; }
+            set { mtu = value; }
+        }
+
+        private bool isAutomaticPrivateAddressingActive;
+
+        public bool IsAutomaticPrivateAddressingActive
+        {
+            get { return isAutomaticPrivateAddressingActive; }
+            set { isAutomaticPrivateAddressingActive = value; }
+        }
+
+        private bool isAutomaticPrivateAddressingEnabled;
+
+        public bool IsAutomaticPrivateAddressingEnabled
+        {
+            get { return isAutomaticPrivateAddressingEnabled; }
+            set { isAutomaticPrivateAddressingEnabled = value; }
+        }
+
+        private bool isForwardingEnabled;
+
+        public bool IsForwardingEnabled
+        {
+            get { return isForwardingEnabled; }
+            set { isForwardingEnabled = value; }
+        }
+
+        private bool usesWins;
+
+        public bool UsesWins
+        {
+            get { return usesWins; }
+            set { usesWins = value; }
+        }
+
+        #endregion
     }
 }
--- NetInfo/Program.cs
+++ NetInfo/Program.cs
@@ -8,8 +8,44 @@ namespace NetInfo
 {
     class Program
     {
+        private static AdapterInfo adapterInfo;
+
         static void Main(string[] args)
         {
+            adapterInfo = AdapterInfo.CreateInstance();
+
+            adapterInfo.listAdapter.ForEach(adapter =>
+            {
+                ShowAdapters(adapter);
+            });
+
+            Console.Read();
+        }
+
+        private static void ShowAdapters(AdapterObject adapter)
+        {
+            Console.WriteLine("====== {0} ======", adapter.Description);
+            Console.WriteLine();
+            Console.WriteLine("  Interface type .................... : {0}", adapter.NetworkInterfaceType);
+            Console.WriteLine("  Physical Address .................. : {0}", adapter.PhysicalAddress);
+            Console.WriteLine("  Is receive only.................... : {0}", adapter.IsReceiveOnly);
+            Console.WriteLine("  Multicast.......................... : {0}", adapter.SupportMulticast);
+            Console.WriteLine();
+            Console.WriteLine("  IP Address ........................ : {0}", adapter.IpAddress);
+            Console.WriteLine("  Subnet     ........................ : {0}", adapter.SubnetMask);
+            Console.WriteLine("  Gateway    ........................ : {0}", adapter.Gateway);
+            Console.WriteLine();
+            Console.WriteLine("  DNS suffix ........................ : {0}", adapter.DnsSuffix);
+            Console.WriteLine("  DNS enabled ....................... : {0}", adapter.IsDnsEnabled);
+            Console.WriteLine("  Dynamically configured DNS ........ : {0}", adapter.IsDynamicDnsEnabled);
+            Console.WriteLine();
+            Console.WriteLine("  Index ............................. : {0}", adapter.Index);
+            Console.WriteLine("  MTU ............................... : {0}", adapter.Mtu);
+            Console.WriteLine("  APIPA active....................... : {0}", adapter.IsAutomaticPrivateAddressingActive);
+            Console.WriteLine("  APIPA enabled...................... : {0}", adapter.IsAutomaticPrivateAddressingEnabled);
+            Console.WriteLine("  Forwarding enabled................. : {0}", adapter.IsForwardingEnabled);
+            Console.WriteLine("  Uses WINS ......................... : {0}", adapter.UsesWins);
+            Console.WriteLine();
         }
     }
 }
