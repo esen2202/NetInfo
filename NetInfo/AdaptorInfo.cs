6b92d5769f2c1cdcb10c7296b8a32c906b13226e
Esen Erkan <erkanesen2202@gmail.com>
2018-11-06 20:25:20 +0300

    Started Project

---
+++ NetInfo/AdaptorInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.NetworkInformation;
+
+namespace NetInfo
+{
+    class AdaptorInfo
+    {
+        static AdaptorInfo adaptorInfo;
+        private static object objLock;
+
+        NetworkInterface[] adapters;
+
+        private AdaptorInfo()
+        {
+
+        }
+
+        public static AdaptorInfo CreateInstance()
+        {
+            if (adaptorInfo != null)
+                lock (objLock)
+                {
+                    if (adaptorInfo != null)
+                        adaptorInfo = new AdaptorInfo();
+                }
+
+            return adaptorInfo;
+        }
+
+
+    }
+}
