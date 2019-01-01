560492b3720f674b5e7468f8c7383e35a0df4f07
Esen Erkan <erkanesen2202@gmail.com>
2019-01-01 23:08:15 +0300

    Sidebar Animation Hide/Show ->  <- Sidebar Collapsed Up | Expand Down

--- NetAda/Views/MainWindow.xaml.cs
+++ NetAda/Views/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
+using System.Windows.Media.Animation;
 
 namespace NetAda.Views
 {
@@ -68,30 +70,98 @@ namespace NetAda.Views
         }
 
         #endregion
-   
+
+        private Storyboard openStoryboard;
+        private Storyboard closeStoryboard;
+
+        private bool pinnedSideBar = false;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width;
-            this.Top = 0;
+            //this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width;
+            this.Left = SystemParameters.WorkArea.Left;
+            this.Top = SystemParameters.WorkArea.Top;
+            this.Width = SystemParameters.WorkArea.Width;
+            this.Height = SystemParameters.WorkArea.Height;
 
-            this.MaxHeight = SystemParameters.WorkArea.Height;
             this.DataContext = new ViewModels.ViewModelMainWindow();
 
-            TopMostIconChanger();
+            this.Topmost = true;
+            PinnedIconChanger();
+            
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //EnableBlur();
+            openStoryboard = new Storyboard();
+            closeStoryboard = new Storyboard();
+
+            openStoryboard.Completed += OpenStoryboard_Completed;
+            closeStoryboard.Completed += CloseStoryboard_Completed;
+        }
+
+        private void CloseStoryboard_Completed(object sender, EventArgs e)
+        {
+            borderOpenSideBar.Visibility = Visibility.Visible;
+        }
+
+        private void OpenStoryboard_Completed(object sender, EventArgs e)
+        {
+            borderOpenSideBar.Visibility = Visibility.Collapsed;
+        }
+
+        private void OpenCloseBorder(Action action)
+        {
+            action?.Invoke();
+        }
+
+        private void OpenBorderAnimation()
+        {
+
+            DoubleAnimation animation = new DoubleAnimation(borderSideBar.Width, 320, TimeSpan.FromMilliseconds(500));
+
+            animation.EasingFunction = new PowerEase();
+            openStoryboard.Children.Add(animation);
+
+            Storyboard.SetTarget(animation, borderSideBar);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(Width)"));
+
+            openStoryboard.Begin();
+        }
+
+        private void CloseBorderAnimation()
+        {
+            DoubleAnimation animation = new DoubleAnimation(borderSideBar.Width, 0, TimeSpan.FromMilliseconds(500));
+            animation.EasingFunction = new PowerEase();
+            closeStoryboard.Children.Add(animation);
+
+            Storyboard.SetTarget(animation, borderSideBar);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(Width)"));
+
+            closeStoryboard.Begin();
+
+        }
+
+        private void BorderSideBar_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            closeStoryboard.Stop();
+            borderOpenSideBar.Visibility = Visibility.Collapsed;
+            OpenCloseBorder(OpenBorderAnimation);
+        }
+
+        private void BorderSideBar_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (borderSideBar.Width >= 320 && !pinnedSideBar)
+                OpenCloseBorder(CloseBorderAnimation);
 
         }
 
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
             //this.Close();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            EnableBlur();
         }
 
         private void BtnNetConnections_Click(object sender, RoutedEventArgs e)
@@ -111,15 +181,14 @@ namespace NetAda.Views
 
         private void BtnAlwaysTop_Click(object sender, RoutedEventArgs e)
         {
-            this.Topmost = !this.Topmost;
-            TopMostIconChanger();
+            pinnedSideBar = !pinnedSideBar;
+            PinnedIconChanger();
         }
 
-        private void TopMostIconChanger()
+        private void PinnedIconChanger()
         {
-            iconPin.Kind = this.Topmost ? MaterialDesignThemes.Wpf.PackIconKind.PinOutline : MaterialDesignThemes.Wpf.PackIconKind.PinOffOutline;
+            iconPin.Kind = pinnedSideBar ? MaterialDesignThemes.Wpf.PackIconKind.PinOutline : MaterialDesignThemes.Wpf.PackIconKind.PinOffOutline;
         }
-
 
         private void LbNetList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
@@ -132,8 +201,18 @@ namespace NetAda.Views
 
         private void BtnShowHideList_OnClick(object sender, RoutedEventArgs e)
         {
-            lbNetList.Visibility =
-                lbNetList.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+            if (lbNetList.Visibility == Visibility.Collapsed)
+            {
+                lbNetList.Visibility = Visibility.Visible;
+                this.SizeToContent = SizeToContent.Manual;
+                this.Height = SystemParameters.WorkArea.Height;
+            }
+            else
+            {
+                this.SizeToContent = SizeToContent.Height;
+                lbNetList.Visibility = Visibility.Collapsed; 
+            }
+
             ShowHideIconChanger();
         }
 
